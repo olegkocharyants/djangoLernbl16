3c897c635e857cc18674fe97144e2aa428eca4b9
olegkocharyants <kocharyants@outlook.com>
2024-07-04 14:57:26 +0300

    бл17_лекция 3_восстановление пароля

--- bl16/settings.py
+++ bl16/settings.py
@@ -102,9 +102,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kocharyants@gmail.com'
+EMAIL_HOST_PASSWORD = 'oqso iezw cyhb xbjs'
+
 
 STATIC_URL = 'static/'
 
--- bl16/urls.py
+++ bl16/urls.py
@@ -11,6 +11,15 @@ urlpatterns = [
     path('profile/', userViews.profile, name='profile'),
     path('user/', authViews.LoginView.as_view(template_name='users/user.html'), name='user'),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
+    
+    path('pass-reset/', authViews.PasswordResetView.as_view(template_name='users/pass_reset.html'), name='pass-reset'),
+
+    path('password_reset_complete/', authViews.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+
+    path('password_reset_confirm/<uidb64>/<token>/', authViews.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    
+    path('password_reset_done/<uidb64>/<token>/', authViews.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+
     path('', include('blog.urls'))
 ]
 
